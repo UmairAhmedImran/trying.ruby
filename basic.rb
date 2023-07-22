0c33dd58a7dcd585407fab280fa1c2926d887661
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-22 21:21:43 +0500

    Creating a basic file to create some basic program

---
+++ basic.rb
@@ -0,0 +1,10 @@
+5.times { print "Umair! " }
+
+exit unless "restaurant".include? "aura"
+
+['toast', 'cheese', 'wine'].each { |food| print food.capitalize}
+
+first_name = "Umair"
+last_name = "Imran"
+
+print first_name + last_name + num
