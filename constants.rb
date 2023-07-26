f881a2f440c7c24b160d4d2004129e1ebd955164
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-26 17:47:40 +0500

    defining variables

---
+++ constants.rb
@@ -0,0 +1,11 @@
+class Example
+    VAR1 = 100
+    VAr2 = 200
+    def sample()
+        puts "Value of variable 1 is #{VAR1}"
+        puts "value of variable 2 is #{VAr2}"
+    end    
+end
+
+exm = Example.new
+exm.sample
---
+++ detail_variables.rb
@@ -0,0 +1,31 @@
+#################### Integer #################################
+a = 123                  # Fixnum decimal
+b = 1_234                # Fixnum decimal with underline
+c = -500                 # Negative Fixnum
+d = 0377                 # octal
+e = 0xff                 # hexadecimal
+f = 0b1011               # binary
+g = ?a                   # character code for 'a'
+h = ?\n                  # code for a newline (0x0a)
+i = 12345678901234567890 # Bignum
+
+puts a
+puts b
+puts c
+puts d
+puts e
+puts f
+puts g
+puts h
+puts i
+
+################# floating numbers ##################
+j = 123.4                # floating point value
+k = 1.0e6                # scientific notation
+l = 4E20                 # dot not required
+m = 4e+20                # sign before exponential
+
+puts j
+puts k
+puts l
+puts m 
