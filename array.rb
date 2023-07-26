71d4655fb68e697ea7c4757f60dcb55d7d774bcc
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-26 20:33:06 +0500

    Adding more basic files

---
+++ array.rb
@@ -0,0 +1,4 @@
+array = ["fried", 3.14, "every data type can be stored in a single array"]
+array.each do |i|
+    puts i
+end
---
+++ string_variables.rb
@@ -0,0 +1,3 @@
+puts "how to get backslah '\\'"
+puts "That\'s right"
+puts "Multiplication value: #{24*40*60}"
