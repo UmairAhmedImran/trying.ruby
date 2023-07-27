dd48ad06503e855b42f4f9290bad339bbb727647
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-27 17:33:03 +0500

    Operators and range

---
+++ operators.rb
@@ -0,0 +1,16 @@
+# only those operators which are different from python
+a = 10
+b = 11
+print (a<=>b) # return 0 if equal, return 1 if left hand side is greater, return -1 if right hand side is greater
+
+print "\n"
+
+print (1..20) === 5 # check if something is present or not
+
+print "\n"
+
+print (1 == 1.0)
+
+print "\n"
+
+print 1.eql?(1.0)
---
+++ range.rb
@@ -0,0 +1,9 @@
+(10..15).each do |n| # .. ptint from start to end
+    print n, " "
+end
+
+print "\n"
+
+(10...15).each do |n| # ... start to finish exclude the last value
+    print n, " "
+end
